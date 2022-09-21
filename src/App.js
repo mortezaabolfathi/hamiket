59383577531536b37c528d4ea13962d4642e462f
Morteza <morteza.abolfathi73@gmail.com>
2022-09-21 10:13:31 +0430

    edit App.js routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PanelAdmin from './components/PanelAdmin/PanelAdmin';
 import LoginPage from './components/LoginPage/LoginPage';
+import Product from './components/Product/Product';
+import HomeLayout from './components/layout';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage/>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>}/>
+            <Route path="/admin" element={<HomeLayout/>}>
+              <Route index element={<PanelAdmin/>}/>     
+              <Route path="products" element={<Product/>}/>         
+            </Route> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
